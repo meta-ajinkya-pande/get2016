d69378396148fd4a4d976a6cd06f4fc4676f9b2a
meta-ajinkya-pande <ajinkya.pande@metcube.com>
2016-07-20 20:42:55 +0530

    Creating directory for OOPS Assignment

---
+++ OOPS-Assignments/Session 1/assignment2/Layout.java
@@ -0,0 +1,20 @@
+package assignment2;
+
+public class Layout {
+	int layoutId;
+	
+	// Use To set different layouts
+	public void setLayout(){	
+	}
+
+	// use to tell which layout set
+	public int getLayoutId() {
+		return layoutId;
+	}
+	
+	// Setting id according to the layout
+	public void setLayoutId(int layoutId) {
+		this.layoutId = layoutId;
+	}
+	
+}
